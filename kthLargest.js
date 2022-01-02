a8a0579866d2bf6efc14b51610d2217d1adaa4f9
VaishnawiDighore <92801895+VaishnawiDighore@users.noreply.github.com>
2022-01-02 20:29:05 +0530

    Add files via upload

---
+++ kthLargest.js
@@ -0,0 +1,5 @@
+let arr=[1,4,5,7];
+let ans = arr.sort();
+let k=1;
+let n= ans.length;
+console.log(ans[n-k]);
---
+++ kthSmallest.js
@@ -0,0 +1,5 @@
+// kth Smallest Element in  sorted array 
+let arr = [16,20,30,80,11,15,2];
+let k=3;
+let ans= arr.sort();
+console.log(ans[k]);
